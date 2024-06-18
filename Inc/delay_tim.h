70c8e408bfcfaddfea365377d80c158d184f26fe
hanghang <86754504+hanghang0219@users.noreply.github.com>
2024-06-18 09:43:51 +0800

    add simulative i2c

---
+++ Inc/delay_tim.h
@@ -0,0 +1,8 @@
+//
+// Created by admin on 17/06/2024.
+//
+
+#ifndef H723_SSR_DELAY_TIM_H
+#define H723_SSR_DELAY_TIM_H
+
+#endif //H723_SSR_DELAY_TIM_H
---
+++ Src/gxhtc3.c
@@ -0,0 +1,3 @@
+//
+// Created by admin on 17/06/2024.
+//
