5e813048de36f93fe513cf20ae4cb6b5aedb82cf
hanghang <86754504+hanghang0219@users.noreply.github.com>
2024-06-18 10:43:35 +0800

    add file

---
+++ Inc/gxhtc3.h
@@ -0,0 +1,10 @@
+//
+// Created by hanghang on 18/06/2024.
+//
+
+#ifndef H723_SSR_GXHTC3_H
+#define H723_SSR_GXHTC3_H
+
+#include "main.h"
+
+#endif //H723_SSR_GXHTC3_H
---
+++ Src/gxhtc3.c
@@ -0,0 +1,11 @@
+//
+// Created by hanghang on 18/06/2024.
+//
+#include "gxhtc3.h"
+#include "cmsis_os.h"
+
+
+HAL_StatusTypeDef initGxhtc3() {
+
+
+}
