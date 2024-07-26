12796d4d67be55a07dd4053b0cb2d6fbd36fdf2f
zh <1480020460@qq.com>
2024-07-26 15:36:55 +0800

    add .clang-format

--- Src/app.c
+++ Src/app.c
@@ -2,17 +2,16 @@
 #include "cmsis_os2.h"
 #include "lps22hb.h"
 
-
 const osThreadAttr_t main_attributes = {
-        .name = "main",
-        .stack_size = 1024 * 4,
-        .priority = (osPriority_t) osPriorityNormal,
+    .name = "main",
+    .stack_size = 1024 * 4,
+    .priority = (osPriority_t)osPriorityNormal,
 };
 
 const osThreadAttr_t thread_attributes = {
-        .name = "main",
-        .stack_size = 256 * 4,
-        .priority = (osPriority_t) osPriorityNormal,
+    .name = "main",
+    .stack_size = 256 * 4,
+    .priority = (osPriority_t)osPriorityNormal,
 };
 
 Lps22hb lps;
@@ -27,11 +26,7 @@ __NO_RETURN void mainThread(void* arg) {
   }
 }
 
-
-__NO_RETURN void threadThread(void* arg) {
-
-
-}
+__NO_RETURN void threadThread(void* arg) {}
 
 void lpsRxCallback(I2C_HandleTypeDef* hi2c) {
   if (lps.flag == LPS22HB_FLAG_ID) {
@@ -49,19 +44,16 @@ void lpsTxCallback(I2C_HandleTypeDef* hi2c) {
 }
 
 void ZMI2CLps22hbInit(Lps22hb* ins) {
-  HAL_I2C_RegisterCallback(ins->i2c, HAL_I2C_MEM_RX_COMPLETE_CB_ID, lpsRxCallback);
-  HAL_I2C_RegisterCallback(ins->i2c, HAL_I2C_MEM_TX_COMPLETE_CB_ID, lpsTxCallback);
+  HAL_I2C_RegisterCallback(ins->i2c, HAL_I2C_MEM_RX_COMPLETE_CB_ID,
+                           lpsRxCallback);
+  HAL_I2C_RegisterCallback(ins->i2c, HAL_I2C_MEM_TX_COMPLETE_CB_ID,
+                           lpsTxCallback);
 }
-
 
 void MX_FREERTOS_Init(void) {
   lps22hbInit(&lps, &hi2c1);
   ZMI2CLps22hbInit(&lps);
 
-
   osThreadNew(mainThread, NULL, &main_attributes);
-//  osThreadNew(threadThread, NULL, &thread_attributes);
+  //  osThreadNew(threadThread, NULL, &thread_attributes);
 }
-
-
-
